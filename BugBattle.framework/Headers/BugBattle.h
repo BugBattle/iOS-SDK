685f1566f6d4b7ad3f1469477d64efa7e0bee761
Lukas Böhler <lukas@boehlerbrothers.com>
2019-01-10 12:30:39 +0100

    Initial release.

---
+++ BugBattle.framework/Headers/BugBattle.h
@@ -0,0 +1,17 @@
+//
+//  BugBattle.h
+//  BugBattle
+//
+//  Created by Lukas on 10.01.19.
+//  Copyright © 2019 BugBattle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BugBattle.
+FOUNDATION_EXPORT double BugBattleVersionNumber;
+
+//! Project version string for BugBattle.
+FOUNDATION_EXPORT const unsigned char BugBattleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugBattle/PublicHeader.h>
