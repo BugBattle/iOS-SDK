19f014f2db45c7862460d30c93affa5b9c0cdd5b
Lukas Böhler <lukas@boehlerbrothers.com>
2019-01-10 14:31:00 +0100

    Cleanup 0.0.5

--- BugBattleDemo/BugBattleDemo/ViewController.swift
+++ BugBattleDemo/BugBattleDemo/ViewController.swift
@@ -14,5 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
+    }
 }
 
