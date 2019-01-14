092625a1adb7348307a19fd0e3daa254ad1dedf9
Lukas Böhler <lukas@boehlerbrothers.com>
2019-01-14 10:43:55 +0100

    Updated demo project to v1.2.9

--- BugBattleDemo/BugBattleDemo/AppDelegate.swift
+++ BugBattleDemo/BugBattleDemo/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        BugBattle.initialize(apiKey: "YOUR-API-KEY", activation: .Shake)
+        BugBattle.initWithToken("YOUR-API-KEY", andActivationMethod: SHAKE)
         
         return true
     }
