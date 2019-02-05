ab2112dfc5123a479352d3aaaf2308b1aee64671
Lukas Böhler <lukas@boehlerbrothers.com>
2019-02-05 13:06:05 +0100

    1.5.1

--- BugBattleDemo/BugBattleDemo/ViewController.m
+++ BugBattleDemo/BugBattleDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <BugBattle/BugBattle.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [BugBattle trackStepWithType: BugBattleStepTypeView andData: @"DemoView"];
+    
+    [BugBattle attachCustomData: @{
+                                   @"test": @"Penis"
+                                   }];
 }
 
 
